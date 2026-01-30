be03663dcf7f8b23f68492907b88e135d48be527
Víctor <adam23vcatala@inslaferreria.cat>
2026-01-30 17:11:37 +0100

    Instalado controlFlow

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,11 +5,12 @@ import { Admin } from './components/admin/admin';
 import { User } from './components/user/user';
 import { DataBinding } from './components/data-binding/data-binding';
 import { SignalEx } from './components/signal-ex/signal-ex';
+import { ControlFlow } from './components/control-flow/control-flow';
 
 
 @Component({
   selector: 'app-root',
-  imports: [Admin,User,DataBinding,SignalEx],
+  imports: [Admin,User,DataBinding,SignalEx,ControlFlow],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
