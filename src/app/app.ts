70542d26c4d7776d09ac3451fbb50c2a47eda166
Víctor <adam23vcatala@inslaferreria.cat>
2026-01-30 17:38:04 +0100

    Importado Router

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RouterLink } from '@angular/router';
 //Importamos los componentes que hemos creado:
 import { Admin } from './components/admin/admin';
 import { User } from './components/user/user';
@@ -10,7 +11,7 @@ import { ControlFlow } from './components/control-flow/control-flow';
 
 @Component({
   selector: 'app-root',
-  imports: [Admin,User,DataBinding,SignalEx,ControlFlow],
+  imports: [Admin, User, DataBinding, SignalEx, ControlFlow, RouterLink, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
