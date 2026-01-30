c64bd568155c9b42ea7710b2d0fc83cc2df56031
Víctor <adam23vcatala@inslaferreria.cat>
2026-01-30 17:21:12 +0100

    Añadido controlFlow

--- src/app/components/control-flow/control-flow.ts
+++ src/app/components/control-flow/control-flow.ts
@@ -1,11 +1,36 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-control-flow',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './control-flow.html',
   styleUrl: './control-flow.css',
 })
 export class ControlFlow {
+  // Atributo para controlar la visibilidad de un párrafo
+  isParaVisible: boolean = true;
+  // Atributo para controlar el mes que usamos
+  startMonthName: String = "feb"
 
+  // Método para mostrar el párrafo
+  showP() {
+    this.isParaVisible = true;
+  }
+
+  // Método para ocultar el párrafo
+  hideP() {
+    this.isParaVisible = false;
+  }
+  //Array de elementos.
+  citList: string[] = ["Pune", "Mumbai", "Panji", "Nagpur"];
+
+  //Array de objetos
+    studentList: any [] = [
+        {name: 'AAAA', city: 'Pune', isActive: false},
+        {name: 'BBB', city: 'Mumbai', isActive: false},
+        {name: 'CCC', city: 'Pune', isActive: true},
+        {name: 'DDD', city: 'Nagpur', isActive: false},
+
+    ]
 }
